e92d2e00139617ea9de457d941ce70ad40916d84
Prajwal Khadgi <prajwalkhadgi11@gmail.com>
2024-10-11 16:05:05 +0545

    feat: add constants for the faq section

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -33,3 +33,43 @@ export const servicesContent = [
       "At GeoSewa, we provide geomatics and IT services, including mapping, surveys, drone services, and digital solutions.",
   },
 ];
+
+export const faqsDataList = [
+  {
+    id: 1,
+    question: "Which data are required for drone scenario analysis?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+  {
+    id: 2,
+    question: "What is metric, parameter, and threshold in MCDA?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+  {
+    id: 3,
+    question:
+      "Can I use iMAP for areas when my data is not uploading. What should I check? mentioned above?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+  {
+    id: 4,
+    question: "Can other info be added to an invoice?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+  {
+    id: 5,
+    question: "How does billing work?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+  {
+    id: 6,
+    question: "How do I change my account email?",
+    answer:
+      "Yes, you can try us for free for 30 days.If you want, we'll provide you with a free, personalized 30-minute onboading call to get you up and running as soon as possible. ",
+  },
+];
