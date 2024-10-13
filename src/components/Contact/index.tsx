42b538beef3651b694266ffb6bc7ad5fd52780dd
Prajwal Khadgi <prajwalkhadgi11@gmail.com>
2024-10-13 11:13:52 +0545

    feat: mobile reponsive for contact page

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,12 +3,12 @@ import ContactMap from "./Map";
 
 export default function Contact() {
   return (
-    <main className="contact naxatw-h-screen-nav naxatw-w-full">
-      <div className="naxatw-flex naxatw-container naxatw-py-14 naxatw-gap-8">
-        <div className="naxatw-w-[55%]">
+    <main className="contact naxatw-w-full">
+      <div className="naxatw-flex naxatw-flex-col md:naxatw-flex-row naxatw-container naxatw-py-6 md:naxatw-py-14 naxatw-gap-8">
+        <div className="naxatw-w-full md:naxatw-w-[55%]">
           <ContactForm />
         </div>
-        <div className="naxatw-w-[45%] naxatw-rounded-xl naxatw-overflow-hidden naxatw-border naxatw-border-primary">
+        <div className="naxatw-w-full md:naxatw-w-[45%] naxatw-rounded-xl naxatw-overflow-hidden naxatw-border naxatw-border-primary">
           <ContactMap />
         </div>
       </div>
